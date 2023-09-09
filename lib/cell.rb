2eaa9730fa0be7d084ae288ec4aeed2566c75050
Joseph Lee <jhjlee702@gmail.com>
2023-09-09 00:22:25 -0600

    write render method' '

--- lib/cell.rb
+++ lib/cell.rb
@@ -4,37 +4,56 @@ class Cell
   def initialize(coordinate)
     @coordinate = coordinate
     @ship = nil
+    @fired_upon = false
   end
 
   def place_ship(ship)
     @ship = ship
-
   end
 
   def empty?
-    if ship == nil 
-      true 
-    else 
-      false
-    end
+    @ship == nil 
   end
   
   def fire_upon
-    if empty? == false
-      ship.hit
-    else 
-      "This square does not have a ship"
-    end
+    @fired_upon = true
+    @ship.hit unless @ship.nil?
   end
 
   def fired_upon?
-    if ship.health == ship.length
-      false 
-    else 
-      true
+    @fired_upon
+  end
+
+  def render(flag = false)
+    if fired_upon?
+      if empty?
+        "M"
+      else
+        if @ship.sunk?
+          "X"
+        else
+          "H"
+        end
+      end
+    else
+      if flag
+        if @ship.nil?
+          "."
+        else
+          "S"
+        end
+      else
+        "."
+      end
     end
   end
 end
 
 #attriibutes: health,
 #methods : fired_upon?
+
+# "." - when render is called upon a cell that has not been fired upon 
+# "M" - if the cell is fired upon and there is no ship on the cell
+# "H" - if the cell is fired upon and there is a ship on the cell
+# "S" -
+# "X" - when the ship is sunk (aka 0 health)
