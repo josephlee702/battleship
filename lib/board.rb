0e02cf76a864357f1d3ad78fd0d8de60f3a4c680
Martin Chavez <martinchavez1203@gmail.com>
2023-09-10 18:11:48 -0600

    Refactor/change Board class and add methods to help valid_placement? method

--- lib/board.rb
+++ lib/board.rb
@@ -1,32 +1,81 @@
 class Board
-  attr_reader :coordinates
-  def initialize
-    @coordinates = {}   
+  attr_reader :cells #attribute reader for the `cells` instance variable, allowing you to access the board's cells from outside the class.
+  def initialize #instance variable `@cells`, which is a hash that represents the board's cells. The default value for each cell is set to `0`.
+    @cells = Hash.new(0)
+    build_board #calls build_board to add cells to the board
   end
-
-  def cells
-    pairs = []
-    letters = ["A" ,"B", "C", "D"]
-    numbers = ["1","2","3","4"]
-    
-    numbers.each do |num|
-      letters.each do |letter|
-        pairs << letter + num 
+  def build_board
+    ('A'..'D').each do |letter| # iterates through the rows ('A' to 'D') and columns (1 to 4) and creates unique coordinates for each cell
+      (1..4).each do |number|
+        coordinate = "#{letter}#{number}"
+        cell_default = Cell.new(coordinate) #initializes a cell with a Cell object and stores it into the @cell hash 
+        @cells[coordinate] = cell_default
       end
     end
-    pairs.sort.each do |pair|
-      @coordinates[pair] = Cell.new(pair)
-    end
-    @coordinates
   end
-  
-  def valid_coordinate?(coordinate)
-    valid_coordinate = coordinates.keys
-    if valid_coordinate.include?(coordinate)
-      true
-    else 
+  #('A'..'D').each do |letter| iterates through each row represented by 'A'- 'D' letters are used to see the letters on the board 
+  #(1..4).each do |number| iteraties through numbers in each row and are used to see the numbers on the board 
+  #coordinate = "#{letter}#{number}" coordinate are made by combinding the row number and letter
+  #
+  def valid_coordinate?(coordinate) #checks if a given coordinate is valid by verifying if it exists in the `@cells` hash
+    @cells.include?(coordinate) #if coordinate in hash return true if not return false
+  end
+
+  def valid_placement?(ship, placement_coordinates) #makes sure placement of the ship is valid acoording to the rules
+    if ship.length != placement_coordinates.length #checks if the length of the ship matches the number of placement coordinates if not return false
       false
+    elsif occupied_cells(placement_coordinates) #checks if the coordinate is occupied by a ship, (occupied_cells) if they are occupied return false
+      false
+    elsif coordinate_horizontal_letters_logic(placement_coordinates) && coordinate_horizontal_numbers_logic(ship, placement_coordinates) || coordinate_vertical_number_logic(placement_coordinates) && coordinate_verticle_letter_logic(ship, placement_coordinates) #checks if they are horizontal or vertical if they do true if not false
+      true
+    else false
     end
   end
 
+  def coordinate_horizontal_letters_logic(placement_coordinates) 
+    letters = placement_coordinates.map do |coordinate| 
+    coordinate[0]
+    end
+    letters.uniq.length == 1
+  end
+
+  def coordinate_horizontal_numbers_logic(ship, placement_coordinates) # takes two arguments: ship, which represents the ship to be placed, and placement_coordinates, which is an array of placement coordinates("A1","A2")# numbers to store the extracted number parts of the coordinates.#uses the map method to iterate through each coordinate in the placement_coordinates array.
+    numbers = placement_coordinates.map do |coordinate| #extracts the second character (the number part) using coordinate[1] converts the number part into an integer. Integers are stored in the numbers array.
+      coordinate[1].to_i #numbers == (numbers.min..numbers.max).to_a checks if the numbers array contains a consecutive range of integers.#(numbers.min..numbers.max) - creates a range of integers from the minimum to the maximum value in the numbers array. example numbers(array) = [1,2,3], the range is [1..3]
+    end# .to_a converts this range into an array. for [1, 2, 3], this part becomes [1, 2, 3]- compares this array to the numbers array. If the numbers array contains consecutive numbers, this condition evaluates to true
+    numbers == (numbers.min..numbers.max).to_a && numbers.length == ship.length #numbers.length == ship.length- checks if the number of elements in the numbers array is equal to the length of the ship, makes sure that the number of coordinates matches the length of the ship
+  end #returns true if both conditions are met, indicating that the placement is valid based on horizontal number logic. if not then it will return false which means that the placement is not valid
+  
+  def coordinate_vertical_number_logic(placement_coordinates)# takes one argument, placement_coordinates, which is an array of placement coordinates("A1",'A2') for a ships vertical placement
+    numbers = placement_coordinates.map do |coordinate| # empty array called numbers to store the extracted number parts of the coordinates.#map method to iterate through each coordinate in the placement_coordinates array.# processes each coordinate in the placement_coordinates array and extracts the second character (the number part) using coordinate[1]
+    coordinate[1].to_i #converts this character into an integer using .to_i,resulting integers (representing the row numbers) are stored in the numbers. Example placement_coordinates = ["A2", "B4", "C3", "D1"] coordinate[1] extracts the second character, looks like coordinate "A2," it extracts "2." then uses .to_i to be integer 2. when all coordinates are proccessed numbers array will look like number = [2,4,3,1]
+    end
+    numbers.uniq.length == 1 #checks if the number of unique row numbers in the numbers array is equal to 1 #number.uniq - returns an array containing only the unique integers from the original numbers array. It removes any duplicate row numbers. #if only one unique row number, this condition evaluates to true, indicating that all the coordinates share the same row number,which means its valid vertical placement
+  end #if more then one unique row, condition will be false because coordinates have diffrent row numbers which is is not valid of a vertical placement 
+
+  def coordinate_verticle_letter_logic(ship, placement_coordinates)#takes two arguments ship, which represents a ship, and placement_coordinates, which is a list of vertical placement coordinates for that ship.
+    letters = placement_coordinates.map do |coordinate| #placement_coordinates an array of strings, where each string represents a coordinate # map iterates through each element of the array and creates a new array,  #creates a new array called letters #iterates through each element in the placement_coordinates list where each element is a coordinate 
+      coordinate[0].ord#ach coordinate, coordinate[0].ord is executed. This means it takes the first character of the coordinate (the letter part) using coordinate[0] and then uses .ord to convert that letter character into its corresponding ASCII code. ASCII = character encoding standard that assigns a unique numerical value which is an iteger.
+    end #for example if the p_coordinates =["A1", "B2","C3"] .map would iterate through each coordinate in the P_coordinate array. Takes the first charcter of the coordinate using coordinate[0]. Example in 'A1' it will take the "A" out in ASCII code "A" = 65. 65 then becomes the result for that specific coordinate in the new array generated by .map # after processing the first coordinate "A1," the letters array will have one element, which is 65. 
+    letters == (letters.min..letters.max).to_a && letters.length == ship.length #letters.min finds the minimum ASCII code value in the letters array. for example [65,66,67] letter.min =65.#letter.max finds max in letters array
+  end #(letters.min..letters.max).to_a - creates an array of consecutive ASCII codes from the minimum to the maximum ASCII code found in the letters array
+  #letters == (letters.min..letters.max).to_a - This checks if the letters array is equal to the array of consecutive ASCII codes, makes sure ASCII codes are contgous if they do = true
+  #letters.length-retrieves the number of elements (ASCII codes) in the letters array tells us how many ASCII codes there are. #ship.length -retrieves the length of the ship.
+  #letters.length == ship.length - checks if the number of elements in the letters array is equal to the length of the ship,  verifies if the number of coordinates in the letters array matches the expected length of the ship
+  #first condition letters == (letters.min..letters.max).to_a checks that the ASCII codes form a contiguous range, making sure that the placement is vertical and consecutive . #second condition = letters.length == ship.length) checks that the number of coordinates matches the ship's length.
+
+  def place(ship, placement_coordinates)  #places a ship on the board by marking the corresponding cells as filled with the type of ship.generates an array representing a contiguous range of ASCII codes.Example =  if letters is [65, 66, 67], this part produces [65, 66, 67], as these are consecutive ASCII codes for 'A', 'B', and 'C'.
+    placement_coordinates.each do |new_coordinate|
+      @cells[new_coordinate].place_ship(ship)
+    end
+  end
+
+  def occupied_cells(placement_coordinates) #checks coordinates are occupied by a ship 
+    placement_coordinates.each do |new_coordinate| #Returns ship object if cell is filled or "nil" if they are not filled up
+      return @cells[new_coordinate].ship
+    end
+  end
+  
 end
+
+
--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -1,3 +1,4 @@
+require 'rspec'
 require './lib/board'
 require './lib/ship'
 require './lib/cell'
@@ -18,11 +19,9 @@ RSpec.describe Board do
 
   describe ' Cell' do 
     it 'creates cells with hash' do 
-      expect(@board.coordinates).to be_a(Hash)
-      expect(@board.coordinates.length).to eq(16)
-      @board.coordinates.each do |coordinate, cell_obj|
-      expect(cell_obj).to be_an_instance_of(Cell)
-      end 
+      expect(@board.cells).not_to eq({})
+      expect(@board.cells.length).to eq(16)
+      
     end
   end
 
@@ -33,6 +32,37 @@ RSpec.describe Board do
       expect(@board.valid_coordinate?("A5")).to be(false)
       expect(@board.valid_coordinate?("E1")).to be(false)
       expect(@board.valid_coordinate?("A22")).to be(false)
+    end
+  end
+
+  describe "#valid_placement? array should be same length" do 
+    it "coordinates in the array should be the same as the length of the ship" do 
+      expect(@board.valid_placement?(@cruiser, ["A2", "A3", "A4"])).to be(true)
+      expect(@board.valid_placement?(@cruiser, ["A1", "A2"])).to be(false)
+      expect(@board.valid_placement?(@submarine, ["A2", "A3", "A4"])).to be(false)
+    end
+  end
+  
+  describe '#Valid_placement? coordinates are conseutive' do 
+    it'makes sure coordinates are consecutive' do 
+      expect(@board.valid_placement?(@cruiser, ["A1", "A2", "A4"])).to be(false)
+      expect(@board.valid_placement?(@submarine, ["A1", "C1"])).to be(false)
+      expect(@board.valid_placement?(@cruiser, ["A3", "A2", "A1"])).to be(false)
+      expect(@board.valid_placement?(@submarine, ["C1", "B1"])).to be(false)
+    end
+  end
+
+  describe '#Valid_placement? coordinates cant be diagonal' do 
+    it'Makes sure coordinates cant be diagonal' do 
+      expect(@board.valid_placement?(@cruiser, ["A1", "B2", "C3"])).to be(false)
+      expect(@board.valid_placement?(@submarine, ["C2", "D3"])).to be(false)
+    end
+  end
+
+  describe '#Valid_placement? If all the previous checks pass then valid' do
+    it'Checks and makes sure the placment is valid' do 
+      expect(@board.valid_placement?(@submarine, ["A1", "A2"])).to be(true)
+      expect(@board.valid_placement?(@cruiser, ["B1", "C1", "D1"])).to be(true)
     end
   end
 end
