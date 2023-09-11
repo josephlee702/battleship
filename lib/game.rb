19c485c7732abcb7e6b75a98bc59422c1a1286ec
Joseph Lee <jhjlee702@gmail.com>
2023-09-11 15:04:35 -0600

    create main menu prompt only

--- lib/game.rb
+++ lib/game.rb
@@ -1,11 +1,27 @@
 class Game
-  attr_reader
-  
-  def initialize()
-    
+
+  def initialize
+  end
+
+  def main_menu
+    puts "Welcome to Battleship. Enter p to play, or enter q to quit."
+    puts "---------------------------------------------------------"
+    # input = gets.chomp
+    # if input == "p"
+    #   setup
+    # else
+    #   input == "q"
+    #   return
+    # end
+  end
+
+  def setup
+    p "hi"
   end
 end
 
 
 #we're going to need a runner file to be able to actually run the game
 #going to need a lot of loops (guessing this is because we have to take turns )
+
+#methods: main menu, setup, turn, end_game
