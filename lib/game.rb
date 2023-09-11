28a08ebd3223688e49a76d5042b044767a7e2db7
Joseph Lee <jhjlee702@gmail.com>
2023-09-11 14:35:26 -0600

    create game files

---
+++ lib/game.rb
@@ -0,0 +1,11 @@
+class Game
+  attr_reader
+  
+  def initialize()
+    
+  end
+end
+
+
+#we're going to need a runner file to be able to actually run the game
+#going to need a lot of loops (guessing this is because we have to take turns )
